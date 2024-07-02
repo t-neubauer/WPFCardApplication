66612d0d0a85c48a1a43ce245eb7c2341e4d7758
tobi1 <tobi1@DESKTOP-NT9PRH8>
2024-07-03 01:03:47 +0200

    Add project files.

---
+++ WPFCardApplication/DataObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCardApplication
+{
+	internal class DataObject
+	{
+        public string CardName { get; set; }
+        public string Rarity { get; set; }
+
+		public DataObject(string cardName, string cardType)
+		{
+			CardName = cardName;
+			Rarity = cardType;
+        }
+    }
+}
---
+++ WPFCardApplication/DataSet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCardApplication
+{
+    internal class DataSet : ObservableCollection<DataObject>
+	{																		
+		string[] names = { "Blue Eyes White Dragon", "Dark Magician", "Time Wizard", "Red Eyes B. Dragon" };
+		string[] types = { "monster", "trap", "spell", "ritual", "synchro", "xyz", "fusion" };
+		string[] edition = { "lob", "mrd", "srl", "mrl", "pgd", "psv", "mfc" };
+		string[] rarity = { "common", "rare", "super rare", "ultra rare", "ultimate rare", "secret rare" };
+
+        public DataSet() : base() 
+        {
+			Random random = new Random();
+			Add(new DataObject(names[random.Next(names.Length)],
+					rarity[random.Next(rarity.Length)]));
+			Add(new DataObject(names[random.Next(names.Length)],
+					rarity[random.Next(rarity.Length)]));
+			Add(new DataObject(names[random.Next(names.Length)],
+					rarity[random.Next(rarity.Length)]));
+			Add(new DataObject(names[random.Next(names.Length)],
+					rarity[random.Next(rarity.Length)]));
+			Add(new DataObject(names[random.Next(names.Length)],
+					rarity[random.Next(rarity.Length)]));
+        }
+	}
+
+	public enum Rarity{ Common, Rare, SuperRare, UltraRare, UltimateRare, SecretRare }
+
+	public static class EnumHelper
+	{
+		public static Array GetValues(Type enumType) {
+			return Enum.GetValues(enumType);	
+		}
+	}
+}
